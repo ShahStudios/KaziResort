2d40c9103306ba53e4d7fe4cb5cfc136285d8568
Shahzada Shah <sshah2@albany.edu>
2020-09-21 23:23:37 -0400

    Created Nav Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
+import Home from './components/pages/Home/Home'
 
 import './App.css'
 
@@ -10,7 +11,7 @@ function App() {
   return (
     <Router>
       <Navbar />
-
+        <Home />
       <Footer />
     </Router>
   )
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,78 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { MdWhatshot } from 'react-icons/md';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
-
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <nav className='navbar'>
+                    <div className='navbar-container container'>
+                        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                            Kazi Resort</Link>
+                        <div className='menu-icon' onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </div>
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                            <li className='nav-item'>
+                                <Link to='/about-us' className='nav-links' onClick={closeMobileMenu}>
+                                    Hotels &amp; Resorts
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link
+                                    to='/services'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                >
+                                    Private Retreats
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link
+                                    to='/services'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                >
+                                    RESIDENCES
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link
+                                    to='/services'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                >
+                                    Shop
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </IconContext.Provider>
         </>
     )
 }
